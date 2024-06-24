1f5f312536473d338a4cacb51f0be1d7808204bc
Tipchan Songtheng <tipchans777@gmail.com>
2024-06-25 00:09:44 +0530

    adding Appwrite ReactNative SDK & config data

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,8 @@ import { Image, ScrollView, Text, View } from "react-native";
 import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import "react-native-url-polyfill/auto";
+
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
 
