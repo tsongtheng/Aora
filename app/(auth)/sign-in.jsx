0c331f4f4d46ca5939850aa8c4edb3ebb8376bab
Tipchan Songtheng <tipchans777@gmail.com>
2024-06-26 01:20:38 +0530

    create context for Authentication

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -8,9 +8,13 @@ import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { signIn } from "../../lib/appwrite";
 
+import { useGlobalContext } from "../../context/GlobalProvider";
+
 const SignIn = () => {
   const [form, setForm] = useState({ email: "", password: "" });
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { setUser, setIsLoggedIn } = useGlobalContext();
 
   const submit = async () => {
     if (!form.email || !form.password) {
@@ -21,7 +25,8 @@ const SignIn = () => {
     try {
       await signIn(form.email, form.password);
 
-      //set it to global state...
+      setUser(result);
+      setIsLoggedIn(true);
 
       router.replace("/home");
     } catch (error) {
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -8,9 +8,13 @@ import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { createUser } from "../../lib/appwrite";
 
+import { useGlobalContext } from "../../context/GlobalProvider";
+
 const SignUp = () => {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { setUser, setIsLoggedIn } = useGlobalContext();
 
   const submit = async () => {
     if (!form.username || !form.email || !form.password) {
@@ -21,7 +25,8 @@ const SignUp = () => {
     try {
       const result = await createUser(form.email, form.password, form.username);
 
-      //set it to global state...
+      setUser(result);
+      setIsLoggedIn(true);
 
       router.replace("/home");
     } catch (error) {
